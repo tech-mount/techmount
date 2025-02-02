a79d074353b2e4b7c33af6369edc9df49f5a9c75
pandraje12 <119981327+pandraje12@users.noreply.github.com>
2025-02-02 13:54:39 -0500

    Comment out unnecessary usings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
